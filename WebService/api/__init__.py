c915b67e0568a1239f4439f4cff351802225b8ab
Aghiles <aghiles.goumeziane@gmail.com>
2018-06-01 03:42:27 +0100

    ajout de L'espace de travail du coté client ViewLayer

--- WebService/api/__init__.py
+++ WebService/api/__init__.py
@@ -9,12 +9,18 @@ from . import authentication, posts, users, comments, errors
 @api.route('/token')
 def get_token(username):
     token=authHandler.generateChallenge('Aghiles')
+    if token is None
+        return unauthorized('Invalid credentials')
+    return jsonify({'token': token})
 
 
 
 @api.route('/authentication')
 def authentifiate():
     token=authHandler.AuthentificateToken(token=token,signature=signature.signature)
+    if token is None
+        return unauthorized('Authentication failed')
+    return jsonify({'token': token})
 
 
 
