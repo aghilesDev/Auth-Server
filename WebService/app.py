1e2cdf6e8658e21d390f156ca4b15a9af74e8dd8
Aghiles <aghiles.goumeziane@gmail.com>
2018-06-01 04:00:48 +0100

    ajout de L'espace de travail du coté client dans WebService/Views/ V3

--- WebService/app.py
+++ WebService/app.py
@@ -1,12 +1,12 @@
 #! /usr/bin/python3.5
-from flask import Flask,Response,session
+from flask import Flask,Response,session,render_template
 from Validator import SignatureValidator
 from TokenGenerator import TokenGenerator as Generator
 
 
 from AuthenticationHandler import AuthenticationHandler as AuthHandler
 from Validator import SignatureProvider as Provider
-app = Flask(__name__)
+app = Flask(__name__,template_folder='Views')
 app.secret_key = '2d9-E2.)f&é,A$p@fpa+zSU03êû9_'
 
 from api import api
@@ -17,7 +17,7 @@ app.register_blueprint(api_1_0_blueprint, url_prefix='/api')
 
 @app.route("/")
 def acceuil():
-	pass
+	return render_template('Accueil.html')
 
 
 
